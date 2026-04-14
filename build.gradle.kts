b753f01a6807de1be97184bd4c970e810495ec70
FrancoMarengo <francomarengo.21@gmail.com>
2026-04-14 08:47:21 -0300

    CI para backend

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     kotlin("jvm") version "2.2.0"
     id("org.springframework.boot") version "3.2.5"
     id("io.spring.dependency-management") version "1.1.4"
+    id("org.jlleitschuh.gradle.ktlint") version "12.1.0"
 }
 
 group = "preq"
