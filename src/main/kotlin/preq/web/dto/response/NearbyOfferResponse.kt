b50fe951db521ee59a74e0db4cf884b08681b8b6
FrancoMarengo <francomarengo.21@gmail.com>
2026-06-06 00:35:18 -0300

    añado cambios para arreglar issues y preparo todo para nearby offers

---
+++ src/main/kotlin/preq/web/dto/response/NearbyOfferResponse.kt
@@ -0,0 +1,38 @@
+package preq.web.dto.response
+
+import preq.enum.LocationType
+import preq.web.dto.projection.NearbyOfferResult
+import java.math.BigDecimal
+
+data class NearbyOfferResponse(
+    val product: ProductResponse,
+    val location: LocationResponse,
+    val distanceMeters: Double,
+    val price: BigDecimal,
+    val averagePrice: BigDecimal,
+) {
+    companion object {
+        fun from(r: NearbyOfferResult) = NearbyOfferResponse(
+            product = ProductResponse(
+                id = r.getProductId(),
+                name = r.getProductName(),
+                brand = r.getProductBrand(),
+                quantity = r.getProductQuantity(),
+                quantityType = r.getProductQuantityType(),
+                minPrice = r.getProductMinPrice(),
+                maxPrice = r.getProductMaxPrice(),
+                barcode = r.getProductBarcode(),
+                images = emptyList(),
+            ),
+            location = LocationResponse(
+                id = r.getLocationId(),
+                name = r.getLocationName(),
+                address = r.getLocationAddress(),
+                type = LocationType.valueOf(r.getLocationType()),
+            ),
+            distanceMeters = r.getDistanceMeters(),
+            price = r.getMinPrice(),
+            averagePrice = r.getAvgPrice(),
+        )
+    }
+}
