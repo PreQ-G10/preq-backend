a4404b1d2cc3e492abcf7cc1ab6e1180da0f6594
FrancoMarengo <francomarengo.21@gmail.com>
2026-06-06 00:35:18 -0300

    añado cambios para arreglar issues y preparo todo para nearby offers

---
+++ src/main/kotlin/preq/web/dto/projection/NearbyOfferResult.kt
@@ -0,0 +1,26 @@
+package preq.web.dto.projection
+
+import java.math.BigDecimal
+
+interface NearbyOfferResult {
+    // product
+    fun getProductId(): Long
+    fun getProductName(): String
+    fun getProductBrand(): String
+    fun getProductQuantity(): BigDecimal
+    fun getProductQuantityType(): String
+    fun getProductMinPrice(): BigDecimal?
+    fun getProductMaxPrice(): BigDecimal?
+    fun getProductBarcode(): String?
+
+    // location
+    fun getLocationId(): Long
+    fun getLocationName(): String
+    fun getLocationAddress(): String
+    fun getLocationType(): String
+
+    // offer
+    fun getMinPrice(): BigDecimal
+    fun getAvgPrice(): BigDecimal
+    fun getDistanceMeters(): Double
+}
